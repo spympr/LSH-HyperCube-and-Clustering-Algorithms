617531ddd2be76595b851ff56b6a4a7e590c327f
Alexandra Apostolopoulou <alexaaposto@gmail.com>
2020-10-19 17:43:02 +0300

    update

--- src/lsh.cpp
+++ src/lsh.cpp
@@ -43,7 +43,7 @@ void Approximate_LSH(infoptr info)
             distances.pop();
         }
         auto end = chrono::high_resolution_clock::now(); 
-        info->tLSH[i] = chrono::duration_cast<chrono::microseconds>(end - start).count();  
+        info->tLSH[i] = chrono::duration_cast<chrono::milliseconds>(end - start).count();  
     }
 
     for(int i=0;i<info->Num_of_Queries;i++)
@@ -67,4 +67,45 @@ void Approximate_LSH(infoptr info)
     }
     delete [] LSH_Distances;
     delete [] LSH_nns;
+}
+
+void Approximate_Range_Search(infoptr info)
+{   
+    for(int i=0;i<info->Num_of_Queries;i++)
+    {
+        item* temp_query = info->Queries_Array[i];
+        priority_queue<int, vector<int>, greater<int>> images; 
+
+        unsigned int gi_query_values[info->L];
+
+        gi_values_of_query(info, gi_query_values, i);
+
+        // for(int o=0;o<info->L;o++)  cout << gi_query_values[o] << " ";
+        // cout <<endl;
+
+        for(int j=0;j<info->L;j++)
+        {
+            if(info->Hash_Tables[j][gi_query_values[j]] != NULL)
+            {
+                int images_in_bucket = info->Hash_Tables[j][gi_query_values[j]]->images.size();
+                // cout << images_in_bucket << endl;
+                
+                for(int p=0; p<images_in_bucket;p++)
+                {
+                    item* temp_image = info->Hash_Tables[j][gi_query_values[j]]->images[p];
+                    if(ManhattanDistance(temp_query,temp_image, info->dimensions) < info->R)
+                    // cout << " hh " << info->Hash_Tables[j][gi_query_values[j]]->images[p] << endl;
+                    images.push((ManhattanDistance(temp_query,temp_image, info->dimensions), (temp_image-info->Images_Array[0])/(info->dimensions*sizeof(item))+1));
+                }
+            }
+        }
+
+        for(int k=0;k<info->N;k++)
+        {
+            Images_Range_Search[i][k] = images.top();
+
+            images.pop();
+        }
+    }
+
 }
