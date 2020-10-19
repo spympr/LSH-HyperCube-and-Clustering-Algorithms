d105fa8edcf68d1bcecf72934a24cfdf43445c11
Spyros Briakos <spympr.6@gmail.com>
2020-10-19 18:22:51 +0300

    update

--- src/lsh.cpp
+++ src/lsh.cpp
@@ -8,14 +8,9 @@ using namespace std::chrono;
 
 void Approximate_LSH(infoptr info)
 {   
-    int** LSH_nns = new int*[info->Num_of_Queries];
-    for(int i=0;i<info->Num_of_Queries;i++)   LSH_nns[i] = new int[info->N];
-        
-    int** LSH_Distances = new int*[info->Num_of_Queries];
-    for(int i=0;i<info->Num_of_Queries;i++)   LSH_Distances[i] = new int[info->N];
-
     for(int i=0;i<info->Num_of_Queries;i++)
     {
+        int LSH_nns[info->N],LSH_Distances[info->N]; 
         auto start = chrono::high_resolution_clock::now(); 
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > distances; 
 
@@ -36,76 +31,64 @@ void Approximate_LSH(infoptr info)
             }
         }
 
-        for(int k=0;k<info->N;k++)
-        {
-            LSH_Distances[i][k] = distances.top().first;
-            LSH_nns[i][k] = distances.top().second;
-            distances.pop();
-        }
         auto end = chrono::high_resolution_clock::now(); 
-        info->tLSH[i] = chrono::duration_cast<chrono::milliseconds>(end - start).count();  
-    }
-
-    for(int i=0;i<info->Num_of_Queries;i++)
-    {
+        
         cout << endl << "--------------------------------------------" << endl;
         cout << "Query: " << i+1 << endl;
-        for(int j=0;j<info->N;j++)
-        {
-            cout << "Nearest neighbor-" << j+1 << ": " << LSH_nns[i][j] << endl;
-            cout << "distanceLSH: " << LSH_Distances[i][j] << endl;
-            cout << "distanceTrue: " << info->True_Distances[i][j] << endl << endl;
-        }
-        cout << "tLSH: " << info->tLSH[i] << "ms" << endl << "tTrue: " << info->tTrue[i] << "ms";
-    }
-    cout << endl << "--------------------------------------------" << endl;
-
-    for(int i=0;i<info->Num_of_Queries;i++)   
-    {
-        delete [] LSH_Distances[i];
-        delete [] LSH_nns[i];
-    }
-    delete [] LSH_Distances;
-    delete [] LSH_nns;
-}
-
-void Approximate_Range_Search(infoptr info)
-{   
-    for(int i=0;i<info->Num_of_Queries;i++)
-    {
-        item* temp_query = info->Queries_Array[i];
-        priority_queue<int, vector<int>, greater<int>> images; 
-
-        unsigned int gi_query_values[info->L];
-
-        gi_values_of_query(info, gi_query_values, i);
-
-        // for(int o=0;o<info->L;o++)  cout << gi_query_values[o] << " ";
-        // cout <<endl;
-
-        for(int j=0;j<info->L;j++)
-        {
-            if(info->Hash_Tables[j][gi_query_values[j]] != NULL)
-            {
-                int images_in_bucket = info->Hash_Tables[j][gi_query_values[j]]->images.size();
-                // cout << images_in_bucket << endl;
-                
-                for(int p=0; p<images_in_bucket;p++)
-                {
-                    item* temp_image = info->Hash_Tables[j][gi_query_values[j]]->images[p];
-                    if(ManhattanDistance(temp_query,temp_image, info->dimensions) < info->R)
-                    // cout << " hh " << info->Hash_Tables[j][gi_query_values[j]]->images[p] << endl;
-                    images.push((ManhattanDistance(temp_query,temp_image, info->dimensions), (temp_image-info->Images_Array[0])/(info->dimensions*sizeof(item))+1));
-                }
-            }
-        }
-
+        cout << "Query: " << (info->Queries_Array[i]-info->Queries_Array[0])/(info->dimensions*sizeof(item))+1 << endl;
+        
         for(int k=0;k<info->N;k++)
         {
-            Images_Range_Search[i][k] = images.top();
-
-            images.pop();
+            LSH_Distances[k] = distances.top().first;
+            LSH_nns[k] = distances.top().second;
+            distances.pop();
+            cout << "Nearest neighbor-" << k+1 << ": " << LSH_nns[k] << endl;
+            cout << "distanceLSH: " << LSH_Distances[k] << endl;
+            cout << "distanceTrue: " << info->True_Distances[i][k] << endl << endl;
         }
+        info->tLSH[i] = chrono::duration_cast<chrono::milliseconds>(end - start).count();  
+        cout << "tLSH: " << info->tLSH[i] << "ms" << endl << "tTrue: " << info->tTrue[i] << "ms";
     }
-
+    cout << endl;
 }
+
+// void Approximate_Range_Search(infoptr info)
+// {   
+//     for(int i=0;i<info->Num_of_Queries;i++)
+//     {
+//         item* temp_query = info->Queries_Array[i];
+//         priority_queue<int, vector<int>, greater<int>> images; 
+
+//         unsigned int gi_query_values[info->L];
+
+//         gi_values_of_query(info, gi_query_values, i);
+
+//         // for(int o=0;o<info->L;o++)  cout << gi_query_values[o] << " ";
+//         // cout <<endl;
+
+//         for(int j=0;j<info->L;j++)
+//         {
+//             if(info->Hash_Tables[j][gi_query_values[j]] != NULL)
+//             {
+//                 int images_in_bucket = info->Hash_Tables[j][gi_query_values[j]]->images.size();
+//                 // cout << images_in_bucket << endl;
+                
+//                 for(int p=0; p<images_in_bucket;p++)
+//                 {
+//                     item* temp_image = info->Hash_Tables[j][gi_query_values[j]]->images[p];
+//                     if(ManhattanDistance(temp_query,temp_image, info->dimensions) < info->R)
+//                     // cout << " hh " << info->Hash_Tables[j][gi_query_values[j]]->images[p] << endl;
+//                     images.push((ManhattanDistance(temp_query,temp_image, info->dimensions), (temp_image-info->Images_Array[0])/(info->dimensions*sizeof(item))+1));
+//                 }
+//             }
+//         }
+
+//         for(int k=0;k<info->N;k++)
+//         {
+//             Images_Range_Search[i][k] = images.top();
+
+//             images.pop();
+//         }
+//     }
+
+// }
