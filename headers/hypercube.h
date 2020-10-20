5e0dbb46a648d41e9048a6f9e54213575ba27656
Spyros Briakos <spympr.6@gmail.com>
2020-10-20 17:06:31 +0300

    update

--- headers/hypercube.h
+++ headers/hypercube.h
@@ -1,3 +1,4 @@
 #include "../headers/exhausting.h"
 
+int hammingDistance(int , int );
 void HyperCube(string ,string ,int ,int ,int ,int ,int ,int );
--- src/hypercube.cpp
+++ src/hypercube.cpp
@@ -1,5 +1,18 @@
 #include "../headers/hypercube.h"
 
+int hammingDistance(int n1, int n2) 
+{ 
+    int x = n1 ^ n2; 
+    int setBits = 0; 
+  
+    while (x > 0) { 
+        setBits += x & 1; 
+        x >>= 1; 
+    } 
+  
+    return setBits; 
+} 
+
 void HyperCube(string input_file, string query_file,int L,int N,int k,int R,int M,int probes)
 {
 
