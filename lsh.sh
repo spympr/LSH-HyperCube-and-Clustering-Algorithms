3a8ab64a7f234107058d23944ebeaf9907c02648
Alexandra Apostolopoulou <alexaaposto@gmail.com>
2020-10-23 12:45:48 +0300

    update

--- lsh.sh
+++ lsh.sh
@@ -1,1 +1,2 @@
-make lsh && ./executable/lsh -d data/train-images-idx3-ubyte -q data/t10k-images-idx3-ubyte -o output -k 4 -L 5 -N 5 -R 10000
+make lsh
+./executable/lsh -d data/train-images-idx3-ubyte -q data/t10k-images-idx3-ubyte -o output -k 4 -L 5 -N 5 -R 10000
