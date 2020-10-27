72e38534a34ed296f5bfd17726299d11fc8b9327
Spyros Briakos <spympr.6@gmail.com>
2020-10-27 19:42:23 +0200

    update

--- scripts/cube.sh
+++ scripts/cube.sh
@@ -1,6 +1,2 @@
 make cube 
-<<<<<<< HEAD
 ./cube -d data/train-images-idx3-ubyte -q data/t10k-images-idx3-ubyte -o output -k 13 -R 1000 -N 1 -probes 40 -M 1000
-=======
-./cube -d data/train-images-idx3-ubyte -q data/t10k-images-idx3-ubyte -o output -k 13 -R 1000 -N 1 -probes 100 -M 5000
->>>>>>> 82eceb04edd2f97914471e9b19a03cfad919d9f1
